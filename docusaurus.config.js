80662c9a783d9683fd8459aab2cbe19a204823be
Shuojen <joker123911@gmail.com>
2025-09-09 21:31:30 +0800

    Update blog content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,7 @@ const config = {
         title: 'Shuojen Blog',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
