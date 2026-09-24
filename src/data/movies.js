3ba83cef8358f223971fd2af7b57528b1c8516ac
Shuo-jen Huang <joker123911@gmail.com>
2026-09-24 17:32:59 +0800

    update

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,4 +1,5 @@
 export const westernMovies = [
+  { title: "天魔：惡之初", score: 6.4, note: "沒看過天魔系列，所以不當起源作品，當成一個獨立作品看，我覺得很無聊，太冗長", poster: "./img/movie/天魔：惡之初.webp", tags: ["2024", "恐怖", "驅魔"] },
   { title: "微笑2", score: 7.1, note: "中規中矩的恐怖片，鬼的設定上有點太強，可以製造很多幻覺太無恥了，jump scare 稍微有點多", poster: "./img/movie/微笑2.webp", tags: ["2024", "恐怖"] },
   { title: "惡靈古堡爆發夜", score: 7.3, note: "還不錯的喪屍電影，惡靈古堡起源故事，劇情直線簡單", poster: "./img/movie/惡靈古堡爆發夜.webp", tags: ["2026", "恐怖"] },
   { title: "十二怒漢", score: 7.7, note: "畫面調度以及劇本都很厲害的一部低成本思辨電影，不愧是經典", poster: "./img/movie/十二怒漢.webp", tags: ["1957", "劇情", "法律"] },
